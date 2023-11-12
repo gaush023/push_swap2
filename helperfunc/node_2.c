b5a2ab082cac7bb6680d631a4e19c37e60ffa7a5
Shuga Agemura <sagemura@c1r22s2.42tokyo.jp>
2023-11-12 20:58:19 +0900

    update

--- helperfunc/node_2.c
+++ helperfunc/node_2.c
@@ -6,7 +6,7 @@
 /*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 14:43:50 by sagemura          #+#    #+#             */
-/*   Updated: 2023/11/11 18:47:25 by sagemura         ###   ########.fr       */
+/*   Updated: 2023/11/12 19:28:46 by sagemura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,4 @@ int	back_size(t_list **stack_b)
 		back_size++;
 	}
 	return (back_size);
-}
-
-t_list	*copy_list(t_list **stack)
-{
-	t_list *tmp;
-	t_list *new;
-	t_list *head;
-
-	tmp = *stack;
-	new = ft_lstnew(tmp->value);
-	head = new;
-	tmp = tmp->next;
-	while (tmp)
-	{
-		new->next = ft_lstnew(tmp->value);
-		new = new->next;
-		tmp = tmp->next;
-	}
-	return (head);
 }
--- helperfunc/sortfunc/bigstacks_sort.c
+++ helperfunc/sortfunc/bigstacks_sort.c
@@ -6,25 +6,104 @@
 /*   By: sagemura <sagemura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:19:08 by sagemura          #+#    #+#             */
-/*   Updated: 2023/11/11 18:58:49 by sagemura         ###   ########.fr       */
+/*   Updated: 2023/11/12 20:57:38 by sagemura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sortfunc.h"
+#include <stdio.h>
 
-t_list	*sort_copylist(t_list **stacks)
+int	transform_int(t_list **stacks, int *array)
 {
+	t_list	*tmp;
+	int		i;
 
+	i = 0;
+	tmp = *stacks;
+	while (tmp)
+	{
+		array[i] = tmp->value;
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	find_stop_pos(int *array, int **flag, int size, int stop_pos)
+{
+	int	i;
+	int	j;
+	int	min_idx;
+	int	flag_n;
+
+	i = 0;
+	flag_n = 0;
+	j = i + 1;
+	while (i < size - 1)
+	{
+		min_idx = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[min_idx] > array[j])
+				min_idx = j;
+			j++;
+		}
+		if (min_idx != i)
+			ft_swap(&array[min_idx], &array[i]);
+		if (i % stop_pos == 0)
+		{
+			flag[0] = array[i];
+			flag_n++;
+		}
+		i++;
+	}
 }
 
 void	bigstacks_sort(t_list **stack_a, t_list **stack_b)
 {
-	int flag;
-	stack_b = NULL;
+	int	**flag;
+	int	flag_n;
+	int	*array;
+	int	size;
+	int	stop_pos;
 
-	t_list *tmp;
-	tmp = sort_copylist(copy_list(stack_a));
-
-	flag = find_stop_pos(stack_a);
-	printf("flag: %d\n", flag);
+	array = (int *)malloc(sizeof(int) * ft_lstsize(stack_a) + 1);
+	if (!array)
+		return (free(array));
+	size = transform_int(stack_a, array);
+	stop_pos = ft_lstsize(stack_a) / 5;
+	flag = (int *)malloc(sizeof(int *) * stop_pos + 1);
+	if (!flag)
+		return (free(flag));
+	size = transform_int(stack_a, array);
+	find_stop_pos(array, flag, size, stop_pos);
+	flag_n = 0;
+	while (flag[flag_n])
+	{
+		printf("%d\n", flag[flag_n][0]);
+		flag_n++;
+	}
+	while (*stack_a)
+	{
+		if (flag[flag_n][0] > (*stack_a)->value)
+			ft_pb(stack_a, stack_b);
+		else
+			ft_ra(stack_a);
+		if (flag[flag_n][0] == (*stack_a)->value)
+		{
+			ft_pb(stack_a, stack_b);
+			flag_n++;
+		}
+		(*stack_a) = (*stack_a)->next;
+	}
 }
